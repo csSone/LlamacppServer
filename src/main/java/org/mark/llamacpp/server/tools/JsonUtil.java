6e6f560a745d2b1fefbb599b2c83ba707a3693d0
IIIIIllllIIIIIlllll <2432896620@qq.com>
2026-01-25 21:25:04 +0800

    修复一下JSON转换导致的mcp调用问题

--- src/main/java/org/mark/llamacpp/server/tools/JsonUtil.java
+++ src/main/java/org/mark/llamacpp/server/tools/JsonUtil.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -14,7 +13,8 @@ import com.google.gson.JsonObject;
 public class JsonUtil {
 	
 	
-	private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	//private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	private static final Gson gson = new Gson();
 	
 	
 	public static String toJson(Object obj) {
